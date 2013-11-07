18fc4c7af6eb5d23cc05545f8c1c2b5f3c2e1f1e
imanfakhar <imanfakhar@gmail.com>
2013-11-07 03:36:52 +0330

    copy to functions.php
    copy 
     require( get_template_directory() . '/footballlig.php' );
    to functions.php for require footballlig.php

---
+++ copy-to-functions.php
@@ -0,0 +1,7 @@
+<?php 
+		//خط زیر را کپی نمایید و در فایل 
+		//functions.php قالب خود قرار دهید 
+		 require( get_template_directory() . '/footballlig.php' );
+		// اگر قالب شما فایلی با این نام نداشت این صفحه را می توانید تغییر نام دهید به 
+		//functions.php
+?>
